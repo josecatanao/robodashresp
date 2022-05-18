a2297d4922d08351b3328b5440383adc80da0cce
josecatanao <51245690+josecatanao@users.noreply.github.com>
2022-05-18 14:31:15 -0300

    ajustando portas

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(/*(process.env.PORT || 5000)*/3000, () => {
+app.listen((process.env.PORT || 5000), () => {
     console.log(`Conectado na porta: ${port}`)
 })
